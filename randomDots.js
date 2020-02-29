5dd22929930b1bdcd7a0933597146b386ecf7264
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-02-29 21:59:20 +0100

    try out canvas dots

--- randomDots.js
+++ randomDots.js
@@ -1,42 +1,81 @@
-// var idleTimer;
-// var idleState = false;
-// var idleWait = 2000;
+var idleState = false;
+var idleWait = 2000;
+var idleTimer = null;
+var dots = [];
 
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function randomDots () {
-  var canvas = document.getElementById("canvas");
-  var context= canvas.getContext("2d");
-  context.canvas.width  = window.innerWidth;
-  context.canvas.height = window.innerHeight;
-  var radius = 10;
-
+function storeRandPos () {
   var xPos = getRandomInt(canvas.width);
   var yPos = getRandomInt(canvas.height);
-  context.beginPath();
-  context.arc(xPos, yPos, radius, 0, Math.PI*2, false);
-  context.fillStyle = 'black';
-      context.fill();
-      context.lineWidth = 2;
-      context.strokeStyle = 'black';
-      context.stroke();
-    // point(xPos, yPos, canvas);
+  dots.push(xPos, yPos);
 }
 
-// function point(x, y, canvas){
-//   canvas.beginPath();
-//   canvas.arc(x, y, 1, 0, 2 * Math.PI, true);
-//   canvas.stroke();
+// function drawDot (xPos, yPos) {
+//   var canvas = document.getElementById("canvas");
+//   var context= canvas.getContext("2d");
+//   context.canvas.width  = window.innerWidth;
+//   context.canvas.height = window.innerHeight;
+//   var radius = 10;
+
+//   context.beginPath();
+//   context.arc(xPos, yPos, radius, 0, Math.PI*2, false);
+//   context.fillStyle = 'black';
+//       context.fill();
+//       context.lineWidth = 2;
+//       context.strokeStyle = 'black';
+//       context.stroke();
 // }
 
+// function drawDots () {
+//   storeRandPos();
+//   for(var i =0; i<dots.length; i+=2) {
+//     drawDot(dots[i], dots[i+1]);
+//   }
+// }
 
-// startTimer();
-// element.onmousemove = stopTimer();
+// function resetDots() {
+//   dots = [];
+// }
+
+function randDots (time) {
+  clearTimeout(idleTimer);
+  if(idleState == true) {
+    drawDots();
+  }
+  idleState = false;
+  idleTimer = setTimeout(function() {
+    resetDots();
+    idleState = true;
+  }, time);
+
+}
+
+function debug(time) {
+  clearTimeout(idleTimer);
+  if (idleState == true) {
+    document.body.style.backgroundColor = "white";
+  }
+  idleState = false;
+  idleTimer = setTimeout(function() {
+    document.body.style.backgroundColor = "black";
+    idleState = true;
+  }, time);
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  var idleTimer = setTimeout(randomDots, 5000);
-  body.onmousemove(clearTimeout(idleTimer));
+
+  //var idleTimer = setInterval (randDots(), 50);
+  // var debug = window.setInterval(console.log("hi"));
+  randDots(1000);
+  document.body.onmousemove = event => {
+    //resetDots();
+    randDots(1000);
+  }
 });
