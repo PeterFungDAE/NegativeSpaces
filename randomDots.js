5ec7c525136d15b5165f664b4d98d951130e1f1e
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-02-29 23:38:43 +0100

    creating random dot when iddle and removing it on mousemove

--- randomDots.js
+++ randomDots.js
@@ -1,45 +1,12 @@
 var idleState = false;
 var idleWait = 2000;
 var idleTimer = null;
-// var dots = [];
 
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function storeRandPos () {
-  var xPos = getRandomInt(canvas.width);
-  var yPos = getRandomInt(canvas.height);
-  dots.push(xPos, yPos);
-}
-
-// function drawDot (xPos, yPos) {
-//   var canvas = document.getElementById("canvas");
-//   var context= canvas.getContext("2d");
-//   context.canvas.width  = window.innerWidth;
-//   context.canvas.height = window.innerHeight;
-//   var radius = 10;
-
-//   context.beginPath();
-//   context.arc(xPos, yPos, radius, 0, Math.PI*2, false);
-//   context.fillStyle = 'black';
-//       context.fill();
-//       context.lineWidth = 2;
-//       context.strokeStyle = 'black';
-//       context.stroke();
-// }
-
-// function drawDots () {
-//   storeRandPos();
-//   for(var i =0; i<dots.length; i+=2) {
-//     drawDot(dots[i], dots[i+1]);
-//   }
-// }
-
-// function resetDots() {
-//   dots = [];
-// }
 
 // function randDots (time) {
 //   clearTimeout(idleTimer);
@@ -57,38 +24,39 @@ function storeRandPos () {
 function debug(time) {
   clearTimeout(idleTimer);
   if (idleState == true) {
-    document.body.style.backgroundColor = "white";
+    // document.body.style.backgroundColor = "white";
+    var iddleDots = document.querySelectorAll("div.iddleDot");
+    iddleDots.forEach(element => element.parentNode.removeChild(element));
   }
   idleState = false;
   idleTimer = setTimeout(function() {
-    document.body.style.backgroundColor = "black";
+
+    createRandomDot();
+
     idleState = true;
   }, time);
 }
 
+function createRandomDot () {
+  var xPos = getRandomInt(window.innerWidth);
+  var yPos = getRandomInt(window.innerHeight);
+  console.log(xPos);
+  var elem = document.createElement("div");
+  elem.className = "dot iddleDot";
+  elem.style.position = "fixed";
+  elem.style.top =  xPos + "px";
+  elem.style.left =  yPos + "px";
+  document.body.appendChild(elem);
+
+
+}
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
-
-  //var idleTimer = setInterval (randDots(), 50);
-  // var debug = window.setInterval(console.log("hi"));
-  //randDots(1000);
+  debug(2000);
   document.body.onmousemove = event => {
-    //resetDots();
-  //randDots(1000);
+    debug(2000);
   }
 
-  document.body.onclick = event => {
-    console.log(event.clientX);
-    var elem = document.createElement("div");
-    var posXtest = 10;
-    elem.className = "dot";
-    elem.style.position = "fixed";
-    elem.style.top =  event.clientX + "px";
-
-    elem.style.left =  event.clientY + "px";
-    document.body.appendChild(elem);
-
-  }
 });
