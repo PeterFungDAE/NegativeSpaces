3556d5a67783a26b97708ab85a5acb85c3716781
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-03-08 11:51:52 +0100

    text and images hover paragraph designing design done

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  imagesOnUnderlineTextHover();
-  displayAbout();
-  idleRandomDots(500);
-  document.body.onmousemove = event => {idleRandomDots(500);}
-
-});
